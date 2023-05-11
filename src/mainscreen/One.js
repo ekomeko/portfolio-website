4fb4b46bc6f3ac9c50783b63f50fbb5dc650515a
ekomeko <ekinefek@gmail.com>
2023-05-11 16:21:00 +0300

    marquee

--- src/mainscreen/One.js
+++ src/mainscreen/One.js
@@ -7,7 +7,7 @@ function One() {
         <div className="one">
             <Marquee>
                 <div className="marquee">
-                <h1>EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN</h1>
+                <h1 className="header">EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN EKIN</h1>
                 </div>
             </Marquee>
         </div>
