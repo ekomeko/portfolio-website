33f00062827553fdc30e9ad0cd97340db1d188bd
ekomeko <ekinefek@gmail.com>
2023-05-21 14:09:49 +0300

    final commit

--- src/mainscreen/two.js
+++ src/mainscreen/two.js
@@ -5,6 +5,25 @@ function Two() {
   return (
     <>
     <div className="two">
+    <div className="tech">
+    <h1 className='headertwo'>Technologys which i can use</h1>
+    </div>
+    <div className="tech__icons">
+    <img className='img1'  src="https://img.icons8.com/color/48/000000/html-5--v1.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/css3.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/javascript--v1.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/react-native.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/bootstrap.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/git.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/github--v1.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/visual-studio-code-2019.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/c-programming.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/bootstrap.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/tailwindcss.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/sass.png"/>
+    <img  src="https://img.icons8.com/color/48/000000/npm.png"/>
+    <img src="https://img.icons8.com/color/48/000000/python--v1.png"/>
+    </div>
     
     </div>
     </>
